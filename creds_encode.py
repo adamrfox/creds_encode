adf553f56e87752e482a94f924452c9d5f85c4f5
adamfox <adamrfox@yahoo.com>
2019-10-14 16:12:46 -0400

    Python 3 compatibility

--- creds_encode.py
+++ creds_encode.py
@@ -2,24 +2,39 @@
 
 import getpass
 import sys
-
+from codecs import encode, getdecoder
 
 user = {}
 passwd = {}
 i = -1
 while True:
     i += 1
-    array = raw_input ("Enter Array type: (blank if done): ")
+    if int(sys.version[0]) < 3:
+        array = raw_input ("Enter Array type: (blank if done): ")
+    else:
+        array = input("Enter Array type (blank if done): ")
     if array == "":
         break
-    user[array] = raw_input("Enter User: ")
+    if int(sys.version[0]) < 3:
+        user[array] = raw_input("Enter User: ")
+    else:
+        user[array] = input("Enter User: ")
     passwd[array] = getpass.getpass("Enter Password: ")
 fp = open(sys.argv[1], "w")
 data = ""
 for x in user.keys():
     ent_s = x + ":" + user[x] + ":" + passwd[x] + "\n"
     data = data + ent_s
-data = data.encode('rot13')
-data = data.encode('uu_codec')
-fp.write (data)
+if int(sys.version[0]) < 3:
+    data = data.encode('rot13')
+    data = data.encode('uu_codec')
+    fp.write(data)
+else:
+    r13 = getdecoder('rot-13')
+    data = r13(data)[0]
+    data = encode(bytes(data, 'utf-8'), 'uu')
+    data = str(data)[2:len(str(data))-3]
+    data_s = str(data).split('\\n')
+    for line in data_s:
+        fp.write(line + '\n')
 fp.close()
